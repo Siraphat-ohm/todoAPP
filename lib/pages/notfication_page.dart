980c17787bab796f5a0432c3418b5a431cbc76b0
Siraphat <siraphat975@gmail.com>
2022-07-16 14:36:00 +0700

    update page

---
+++ lib/pages/notfication_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificatoinPage extends StatefulWidget {
+  const NotificatoinPage({ Key? key }) : super(key: key);
+
+  @override
+  State<NotificatoinPage> createState() => _NotificatoinPageState();
+}
+
+class _NotificatoinPageState extends State<NotificatoinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
